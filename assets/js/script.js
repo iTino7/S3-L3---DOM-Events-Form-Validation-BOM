4dc1a0b589c8feff3933d61f04038be8ee222994
iTino07 <Sabatino.b007@outlook.it>
2025-03-27 08:42:49 +0100

    manca ancora lo style ma ho giocato anche un po' con le funzioni all'esterno del form.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,68 @@
 // FORM
 
-const form = document.querySelector("form");
+// const form = document.querySelector("form");
 
-form.onsubmit = (e) => {
+// form.onsubmit = (e) => {
+//   e.preventDefault();
+
+//   const inputAdd = document.getElementById("add");
+
+//   const input = {
+//     name: inputAdd.value,
+//   };
+
+//   const containerList = document.getElementById("containerList");
+
+//   const div = document.createElement("div");
+//   div.classList.add("card");
+
+//   const button = document.createElement("button");
+//   button.innerHTML = "Elimina";
+
+//   button.onclick = (e) => {
+//     e.currentTarget.closest(".card").remove();
+//   };
+
+//   const ul = document.createElement("ul");
+//   const li = document.createElement("li");
+
+//   li.innerHTML = inputAdd.value;
+
+//   ul.append(li);
+//   div.append(ul, button);
+//   containerList.append(div);
+
+//   form.reset();
+// };
+
+// TITLE
+
+const h1 = document.querySelector("h1");
+h1.innerHTML = "TO DO LIST";
+
+const deleteIcon = `<i class="far fa-trash-alt"></i>`;
+
+const deleteButton = () => {
+  const button = document.createElement("button");
+  button.innerHTML = deleteIcon;
+  button.onclick = (e) => {
+    e.currentTarget.closest(".card").remove();
+  };
+
+  return button;
+};
+
+// FORM
+
+const containerForm = document.querySelector("form");
+
+containerForm.onsubmit = (e) => {
   e.preventDefault();
 
-  const inputAdd = document.getElementById("add");
+  const containerAdd = document.getElementById("add");
 
-  const input = {
-    name: inputAdd.value,
+  const add = {
+    input: containerAdd.value,
   };
 
   const containerList = document.getElementById("containerList");
@@ -16,21 +70,14 @@ form.onsubmit = (e) => {
   const div = document.createElement("div");
   div.classList.add("card");
 
-  const button = document.createElement("button");
-  button.innerHTML = "Elimina";
-
-  button.onclick = (e) => {
-    e.currentTarget.closest(".card").remove();
-  };
+  deleteButton();
 
   const ul = document.createElement("ul");
   const li = document.createElement("li");
+  li.innerHTML = containerAdd.value;
 
-  li.innerHTML = inputAdd.value;
-
-  ul.append(li);
-  div.append(ul, button);
-  containerList.append(div);
-
-  form.reset();
+  ul.appendChild(li);
+  div.append(ul, deleteButton());
+  containerList.appendChild(div);
+  containerForm.reset();
 };
